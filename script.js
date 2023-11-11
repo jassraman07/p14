8d0b81bb7f4a5bfa771aa13fa0cbd59eacb572dd
jassraman07 <142134609+jassraman07@users.noreply.github.com>
2023-11-11 15:13:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+//Question reverse a string but i am unable to solve this problem still uoloading as a p13
+
+
+
+// const str = "hello";
+// const reversed = reverseStr(str);
+
+// console.log(reversed); 
+ function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("hello"));
+reverseString("hello");
+
